ff64950387d4532db0082139f2b463693e1a9e4c
Your Name <you@example.com>
2023-03-08 22:57:55 +0200

    EP.2

--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -4,15 +4,83 @@ using UnityEngine;
 
 public class Planet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Range(2, 256)]
+    public int resolution = 10;
+    public bool autoUpdate = true;
+    public ShapeSettings shapeSettings;
+    public ColorSettings colorSettings;
+    [HideInInspector]
+    public bool shapeSettingsFoldout;
+    [HideInInspector]
+    public bool colourSettingsFoldout;
+    
+    ShapeGenerator shapeGenerator;
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField, HideInInspector] 
+    MeshFilter[] meshFilters;
+    TerrainFace[] terrainFaces;
+    
+    //void Start()
+    //{
+    //    Initialize();
+    //    GenerateMesh();
+    //}
+    void Initialize()
     {
-        
+        shapeGenerator = new ShapeGenerator(shapeSettings);
+        if (meshFilters == null || meshFilters.Length == 0)
+        {
+            meshFilters = new MeshFilter[6];
+        }
+        terrainFaces = new TerrainFace[6];
+
+        Vector3[] directions = { Vector3.up, Vector3.down, Vector3.left, Vector3.right, Vector3.forward, Vector3.back };
+
+        for (int i = 0; i < 6; i++)
+        {
+            if (meshFilters[i] == null)
+            {
+                GameObject meshObj = new GameObject("mesh");
+                meshObj.transform.parent = transform;
+
+                meshObj.AddComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Universal Render Pipeline/Lit"));
+                meshFilters[i] = meshObj.AddComponent<MeshFilter>();
+                meshFilters[i].sharedMesh = new Mesh();
+            }
+
+            terrainFaces[i] = new TerrainFace(shapeGenerator, meshFilters[i].sharedMesh, resolution, directions[i]);
+        }
+    }
+    public void GeneratePlanet()
+    {
+        Initialize();
+        GenerateMesh();
+        GenerateColours();
+    }
+    public void OnShapeSettingsUpdate()
+    {
+        if (!autoUpdate) return;
+        Initialize();
+        GenerateMesh();
+    }
+    public void OnColourSettingsUpdated()
+    {
+        if (!autoUpdate) return;
+        Initialize();
+        GenerateColours();
+    }
+    void GenerateMesh()
+    {
+        foreach (TerrainFace face in terrainFaces)
+        {
+            face.ConstructMesh();
+        }
+    }
+    void GenerateColours()
+    {
+        foreach (var m in meshFilters)
+        {
+            m.GetComponent<MeshRenderer>().sharedMaterial.color = colorSettings.planetColor;
+        }
     }
 }
